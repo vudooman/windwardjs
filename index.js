29b1586ccec07e7fc066fda585cf02656c97aa9f
Vu Tran <vtran@xactlycorp.com>
2014-09-10 12:41:57 -0700

    Initial files

---
+++ index.js
@@ -0,0 +1,12 @@
+var api = {};
+
+var connect = function(userId, deviceId, done) {
+  var deviceInfo = {};
+  var err = {};
+
+  done(deviceInfo, err);
+};
+
+api.connect = connect;
+
+module.exports = api;
