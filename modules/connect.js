9225ed9c64c546cb22d222df3a6a5e2aa15eb9dc
Vu Tran <vtran@xactlycorp.com>
2014-09-11 12:05:25 -0700

    Added status and error API calls

--- modules/connect.js
+++ modules/connect.js
@@ -1,13 +1,55 @@
 var http = require('http');
 
+var errorsContext = '/errors';
+var devicesContext = '/connectedDevices';
+
 module.exports = function(api, config) {
+	/**
+	 * Connect to windward service. Also store device info for other API usage.
+	 */
 	api.connect = function(userId, deviceId, done) {
-		api.post('/connectedDevices', {
+		api.post(devicesContext, {
 			userId : userId,
 			deviceId : deviceId
 		}, function(info, error) {
 			config.deviceInfo = info;
 			done(info, error);
+			if (error) {
+				api.error(error);
+			}
+		});
+	};
+
+	/**
+	 * Get latest device info
+	 */
+	api.status = function(done) {
+		api.put(devicesContext, config.deviceInfo, function(info, error) {
+			if(info) {
+				config.deviceInfo = info;
+			}
+			done(info, error);
+			if (error) {
+				api.error(error);
+			}
+		});
+	};
+
+	api.error = function(errorObj) {
+
+		if (config.isDebug) {
+			console.log("Error:");
+			console.log(errorObj);
+		}
+
+		api.post(errorsContext, {
+			connectId : config.deviceInfo.connectId,
+			error : errorObj
+		}, function(info, error) {
+			if (error && config.isDebug) {
+				console.log("Could not log error: ");
+				console.log(error);
+			}
 		});
 	};
 };
