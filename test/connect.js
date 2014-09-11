ac2398f7b837fc65baf7eeba909978b6365a83a0
Vu Tran <vtran@xactlycorp.com>
2014-09-11 12:06:27 -0700

    Added status test

--- test/connect.js
+++ test/connect.js
@@ -1,11 +1,17 @@
 var windward = require('../index.js');
 
+windward.debug(true);
+
+console.log("Connecting ...");
 windward.connect('540f8c6340bed70000165f80', '54111605e668ad675c000000',
 		function(info, err) {
-			if (err) {
-				console.log("Error: ");
-				console.log(err);
-			} else {
-				windward.printConfig();
+			if (!err) {
+				console.log("Connected!!");
+				console.log("Updating status ...");
+				windward.status(function(info, error) {
+					if (!error) {
+						console.log("Updated!!");
+					}
+				});
 			}
 		});
