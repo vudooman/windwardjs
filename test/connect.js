c650785671cec4c95591f3b52e294bde4ab4c827
Vu Tran <vudooman@gmail.com>
2014-09-10 22:56:37 -0700

    Update tests to match features

--- test/connect.js
+++ test/connect.js
@@ -1,5 +1,10 @@
 var windward = require('../index.js');
 
-windward.connect('testUser', 'testDevice', function(info, err) {
-	console.log('hi');
+windward.connect('540f8c6340bed70000165f80', '54111605e668ad675c000000', function(info, err) {
+  if(err) {
+    console.log("Error: ");
+    console.log(err);
+  } else {
+    windward.printConfig();
+  }
 });
--- test/report.js
+++ test/report.js
@@ -1,10 +1,17 @@
 var windward = require('../index.js');
 
-windward.reportClimate({
-  humidity: 45,
-  vudoo: 123
-})
-.temperature(123)
-.send(function(info, err) {
-	console.log('hi');
+windward.connect('540f8c6340bed70000165f80', '54111605e668ad675c000000', function(info, err) {
+  if(err) {
+    console.log("Error: ");
+    console.log(err);
+  } else {
+    windward.reportClimate({
+      humidity: 45,
+      dirt: 123
+    })
+    .temperature(123)
+    .send(function(info, err) {
+      console.log(info);
+    });
+  }
 });
